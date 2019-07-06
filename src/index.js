c99a64fc15715a9a19d1673e7764b62e185a5669
israil1601 <israil.salikhodjaev@gmail.com>
2019-07-06 15:13:50 +0500

    solved

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+
+  const result = []
+
+  preferences.forEach((iLove, index) => {
+      const myNum = index + 1
+      const third = preferences[iLove - 1]    
+
+      if (preferences[third - 1] === myNum && third !== iLove) {
+          result.push([myNum, iLove, third])
+      }
+  });
+
+  return result.length / 3
+}
+
+
